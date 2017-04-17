8c0f08ca34baa89120dc01adbba6a35ea7807e37
Qinwen Shi <qinwen.shi@gmail.com>
2017-04-17 14:43:51 +0800

    add init script

---
+++ train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import re
+import string
+import random
+
+with io.open('words.txt','r',encoding='utf8') as f:
+    text = f.read()
+
+words = re.split('\W+', text, flags=re.UNICODE)
+
+for i in range(0, 10):
+  print random.choice(words)
