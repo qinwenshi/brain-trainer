ab22ea4f7526292010aa7a5c809efbcf8428a90d
Qinwen Shi <qinwen.shi@gmail.com>
2017-04-17 14:54:54 +0800

    pass numbers from commandline. default 10

--- train.py
+++ train.py
@@ -5,11 +5,15 @@ import io
 import re
 import string
 import random
+import sys
 
+count = 10
+if len(sys.argv)>1:
+  count = int(sys.argv[1])
 with io.open('words.txt','r',encoding='utf8') as f:
     text = f.read()
 
 words = re.split('\W+', text, flags=re.UNICODE)
 
-for i in range(0, 10):
+for i in range(0, count):
   print i+1, random.choice(words)
